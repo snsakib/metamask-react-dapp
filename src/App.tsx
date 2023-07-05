fd48738ca49dcdf2e60580d7c29676c4e4bfb94d
snsakib <hello@snsakib.com>
2023-07-06 01:05:54 +0600

    detect provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import detectEthereumProvider from '@metamask/detect-provider'
 
 const App = () => {
+  const [hasProvider, setHasProvider] = useState<boolean | null>(null)
+
+  useEffect(() => {
+    const getProvider = async () => {
+      const provider = await detectEthereumProvider({ silent: true });
+      console.log(provider);
+      setHasProvider(Boolean(provider))
+    }
+
+    getProvider();
+  }, [])
 
   return (
     <div className="App">
-      <button>Connect MetaMask</button>
+      {
+        hasProvider && <button>Connect MetaMask</button>
+      }
     </div>
   )
 }
