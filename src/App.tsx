d4d79577a6e9f96159e357c47fb8f630796dc384
snsakib <hello@snsakib.com>
2023-07-07 00:39:31 +0600

    handle state change

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,62 @@ import detectEthereumProvider from "@metamask/detect-provider";
 
 const App = () => {
   const [hasProvider, setHasProvider] = useState<boolean | null>(null);
-  const [wallet, setWallet] = useState(null);
-  const [chainId, setChainId] = useState(null);
+  const initialState = { accounts: [], balance: "" };
+  const [wallet, setWallet] = useState(initialState);
   const targetChainId = "0x1"; // Ethereum Mainnet
 
   useEffect(() => {
+    const refreshAccounts = (accounts: any, balance: any) => {
+      if (accounts.length > 0) {
+        console.log("wallet: " + JSON.stringify(wallet));
+        setWallet((prevState) => ({
+          ...prevState,
+          accounts: accounts,
+          balance: balance,
+        }));
+      } else {
+        setWallet(initialState);
+      }
+    };
+
     const getProvider = async () => {
       const provider = await detectEthereumProvider({ silent: true });
-      console.log(provider);
       setHasProvider(Boolean(provider));
+
+      if (provider) {
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+
+        let balance = await window.ethereum.request({
+          method: "eth_getBalance",
+          params: [accounts[0], "latest"],
+        });
+
+        refreshAccounts(accounts, balance);
+
+        window.ethereum.on("accountsChanged", refreshAccounts);
+      }
     };
 
     getProvider();
+
+    return () => {
+      window.ethereum.removeListener("accountsChanged", refreshAccounts);
+    };
   }, []);
 
   const connectWallet = async () => {
     let accounts = await window.ethereum.request({
       method: "eth_requestAccounts",
     });
-    setWallet(accounts);
-    console.log(wallet);
+    let balance = await window.ethereum.request({
+      method: "eth_getBalance",
+      params: [accounts[0], "latest"],
+    });
+    setWallet({ accounts, balance });
     switchNetwork(targetChainId);
+    console.log("wallet " + JSON.stringify(wallet));
   };
 
   const switchNetwork = async (targetChainId) => {
@@ -63,6 +98,12 @@ const App = () => {
   return (
     <div className="App">
       {hasProvider && <button onClick={connectWallet}>Connect Wallet</button>}
+      {wallet.accounts.length > 0 && (
+        <>
+          <div>Account Address: {wallet.accounts[0]}</div>
+          <div>Wallet Balance: {wallet.balance}</div>
+        </>
+      )}
     </div>
   );
 };
